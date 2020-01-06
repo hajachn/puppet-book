a5a7a0b0803af2ebcebccbffff88dc630db3130c
Haja Moideen <haja@zoozocloud.com>
2020-01-06 12:48:28 +0000

    Added sudo

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo cd /etc/puppetlabs/code/environments/production && git pull
+cd /etc/puppetlabs/code/environments/production && sudo git pull
 sudo /opt/puppetlabs/bin/puppet apply manifests/
